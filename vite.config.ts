734fe1da1b5d6af9973c8ec67ae42b2757c7dd18
slava <vgorovoi@gmail.com>
2025-08-15 17:32:21 +0300

    falcon

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [tailwindcss(), sveltekit()],
+  server: {
+    allowedHosts: ['derivative-game.com', '176.124.145.49'], // add your domain
+	
+    host: true // allows binding to all IPs (0.0.0.0)
+  }
 });
+
+
+
